2bdfb487bd8e831d1cf774c6310da7bc6c63e9fb
alexandra <alexandraschurman@gmail.com>
2021-11-11 19:51:11 -0800

    commit tree c56b4938b5e0c8a8beb7ae48443a5c6263b7a13f

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 const init = () => {
+  const inputForm = document.querySelector('form');
+
+  inputForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const input = document.querySelector('input#searchByID')
+        console.log(e);
+  })
   
+//   e.target.children[1].value
+
+
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
